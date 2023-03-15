6b1b2c4808e755458ad3e6943490898473f28182
MSR333UA <matviivserhii@gmail.com>
2023-03-16 00:53:59 +0200

    refactored to mongoose

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,16 +1,15 @@
 const {RequestError} = require('../helpers');
-const contacts = require('../models/contacts');
-const {addSchema} = require('../schemas/schema');
+const Contact = require('../models/contacts');
+const {addSchema, updateFavoriteSchema} = require('../schemas/schema');
 
 const getAll = async (req, res) => {
-  const result = await contacts.listContacts();
+  const result = await Contact.find();
   res.json(result);
 };
 
 const getById = async (req, res) => {
   const {contactId} = req.params;
-  const result = await contacts.getContactById(contactId);
-
+  const result = await Contact.findById(contactId);
   if (!result) {
     throw new RequestError(404, 'Not found');
   }
@@ -21,7 +20,7 @@ const getPost = async (req, res) => {
   const {error} = addSchema.validate(req.body);
   if (error) throw new RequestError(400, 'missing required name field');
 
-  const result = await contacts.addContact(req.body);
+  const result = await Contact.create(req.body);
   res.status(201).json(result);
 };
 
@@ -29,18 +28,36 @@ const updateById = async (req, res) => {
   const {error} = addSchema.validate(req.body);
   if (error) throw new RequestError(400, 'missing fields');
   const {contactId} = req.params;
-  const result = await contacts.updateContact(contactId, req.body);
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) throw new RequestError(404, 'Not Found');
+  res.json(result);
+};
 
+const updateStatusContact = async (req, res) => {
+  const {error} = updateFavoriteSchema.validate(req.body);
+  if (error) throw new RequestError(400, 'missing field favorite');
+  const {contactId} = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
   if (!result) throw new RequestError(404, 'Not Found');
   res.json(result);
 };
 
 const deleteById = async (req, res) => {
   const {contactId} = req.params;
-  const result = await contacts.removeContact(contactId);
-
+  const result = await Contact.findByIdAndRemove(contactId);
   if (!result) throw new RequestError(404, 'Not found');
   res.json({message: 'contact deleted'});
 };
 
-module.exports = {getAll, getById, getPost, deleteById, updateById};
+module.exports = {
+  getAll,
+  getById,
+  getPost,
+  deleteById,
+  updateById,
+  updateStatusContact,
+};
--- models/contacts.js
+++ models/contacts.js
@@ -1,56 +1,25 @@
-const fs = require('fs/promises');
-const path = require('path');
+const {Schema, model} = require('mongoose');
 
-const contactsPath = path.join(__dirname, './contacts.json');
+const contactSchema = new Schema(
+    {
+      name: {
+        type: String,
+        required: [true, 'Set name for contact'],
+      },
+      email: {
+        type: String,
+      },
+      phone: {
+        type: String,
+      },
+      favorite: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    {versionKey: false, timestamps: true},
+);
 
-const listContacts = async () => {
-  const contacts = await fs.readFile(contactsPath, 'utf-8');
-  return JSON.parse(contacts);
-};
+const Contact = model('contact', contactSchema);
 
-const update = async (contacts) => {
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), 'utf-8');
-  return contacts;
-};
-
-const getContactById = async (contactId) => {
-  const contacts = await listContacts();
-  const contact = contacts.find((contact) => contact.id === contactId);
-  return contact || null;
-};
-
-const removeContact = async (contactId) => {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((contact) => contact.id === contactId);
-  if (index === -1) return null;
-
-  const [removeContact] = contacts.splice(index, 1);
-  await update(contacts);
-  return removeContact;
-};
-
-const addContact = async ({name, email, phone}) => {
-  const contacts = await listContacts();
-  const newContact = {id: String(contacts.length + 1), name, email, phone};
-  contacts.push(newContact);
-  await update(contacts);
-  return newContact;
-};
-
-const updateContact = async (contactId, {name, email, phone}) => {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((contact) => contact.id === contactId);
-  if (index === -1) return null;
-
-  contacts[index] = {id: contactId, name, email, phone};
-  await update(contacts);
-  return contacts[index];
-};
-
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-};
+module.exports = Contact;
